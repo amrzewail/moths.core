a85afee5e4215f49837392bf170cb2b1de0e6ea3
amrzewail <amrzewail@hotmail.com>
2026-05-26 20:41:03 +0300

    interface reference serialization

--- Runtime/Serialization/InterfaceReference.cs
+++ Runtime/Serialization/InterfaceReference.cs
@@ -20,5 +20,33 @@ namespace Moths.Serialization
         public static implicit operator T(InterfaceReference<T> reference) => reference.Value;
 
         public static implicit operator bool(InterfaceReference<T> reference) => reference._object != null && reference.Value != null;
+
+        [System.Serializable]
+        private struct SerializationData
+        {
+            public string type;
+            public string obj;
+        }
+
+        public string Serialize()
+        {
+            if (_object == null) return "{}";
+            SerializationData data;
+            data.type = _object.GetType().AssemblyQualifiedName;
+            data.obj = JsonUtility.ToJson(_object);
+            return JsonUtility.ToJson(data);
+        }
+        public void Deserialize(string json)
+        {
+            if (json == "{}")
+            {
+                _object = null;
+                return;
+            }
+            SerializationData data = JsonUtility.FromJson<SerializationData>(json);
+            _object = JsonUtility.FromJson(data.obj, Type.GetType(data.type));
+        }
+            
+        public void Copy(InterfaceReference<T> copyFrom) => Deserialize(copyFrom.Serialize());
     }
 }
