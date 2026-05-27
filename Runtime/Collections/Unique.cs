c39503f2f099edddc3c524e15ed10885bb87bf62
amrzewail <amrzewail@hotmail.com>
2026-05-28 02:57:45 +0300

    Unique operators

--- Runtime/Collections/Unique.cs
+++ Runtime/Collections/Unique.cs
@@ -1,10 +1,11 @@
 using Moths.Attributes;
+using System;
 using UnityEngine;
 
 namespace Moths.Collections
 {
     [System.Serializable]
-    public struct Unique
+    public struct Unique : IEquatable<Unique>, IEquatable<long>
     {
         private static Unique _empty = new Unique();
 
@@ -17,8 +18,20 @@ namespace Moths.Collections
             return obj is Unique unique && unique._identifier == _identifier;
         }
 
+        public bool Equals(long other)
+        {
+            return _identifier == other;
+        }
+
+        public bool Equals(Unique other)
+        {
+            return _identifier == other._identifier;
+        }
+
         public override string ToString() => _identifier.ToString();
 
+
+        public static implicit operator long(Unique unique) => unique._identifier;
         public static bool operator == (Unique unique1, Unique unique2) => unique1.Equals(unique2);
         public static bool operator !=(Unique unique1, Unique unique2) => !unique1.Equals(unique2);
 
