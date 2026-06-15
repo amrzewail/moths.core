9285c4cbe7b2ba71bcd868adf6ab2553c74f62e1
amrzewail <amrzewail@hotmail.com>
2026-06-15 22:40:59 +0300

    LString multi line

--- Editor/Collections/LStringDrawer.cs
+++ Editor/Collections/LStringDrawer.cs
@@ -1,10 +1,12 @@
+using Moths.Attributes;
+using System.Reflection;
 using UnityEditor;
 using UnityEngine;
 
 namespace Moths.Collections
 {
     [CustomPropertyDrawer(typeof(LString), true)]
-    public class StringDrawer : PropertyDrawer
+    public class LStringDrawer : PropertyDrawer
     {
         public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
         {
@@ -40,7 +42,8 @@ namespace Moths.Collections
             else
             {
                 var text = property.FindPropertyRelative("_text");
-                EditorGUI.PropertyField(valueRect, text, GUIContent.none);
+                valueRect.height = GetTextAreaHeight(text.stringValue);
+                text.stringValue = EditorGUI.TextArea(valueRect, text.stringValue);
             }
 
             EditorGUI.EndProperty();
@@ -55,10 +58,18 @@ namespace Moths.Collections
             {
                 return EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_localization"), label, true) - 2;
             }
-            else
-            {
-                return EditorGUI.GetPropertyHeight(property.FindPropertyRelative("_text"), label, true);
-            }
+
+            var text = property.FindPropertyRelative("_text");
+
+            return GetTextAreaHeight(text.stringValue);
+        }
+
+        private float GetTextAreaHeight(string text)
+        {
+            int lines = Mathf.Max(1, text.Split('\n').Length);
+            lines = Mathf.Clamp(lines, 1, 8);
+
+            return lines * EditorGUIUtility.singleLineHeight - 3.5f * (lines - 1);
         }
     }
 }
